f90e7d162269815d12a32faa114e11234fca7ac8
Taha <iqbaltaha3@gmail.com>
2026-02-06 10:49:27 +0530

    project structure ready-env set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Taha Iqbal",
+    author_email="iqbaltaha3@gmail.com",
+    packages=find_packages()
+)
